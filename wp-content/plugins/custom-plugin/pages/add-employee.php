762cbe616d4357f21c143e6147df4694e44b1f41
Faiz ul Hassan <104769534+faizulhassan11@users.noreply.github.com>
2024-12-27 22:19:47 +0500

    video 10 complete add and list page design complete and datatable added

--- wp-content/plugins/custom-plugin/pages/add-employee.php
+++ wp-content/plugins/custom-plugin/pages/add-employee.php
@@ -1,54 +1,69 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Add Employee</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 </head>
+
 <body>
- 
-<div class="container">
-  <h2>Add Employee</h2>
-  <div class="panel panel-primary">
-    <div class="panel-heading">Add Employee</div>
-    <div class="panel-body">
-    <form action="/action_page.php">
-    <div class="mb-3 mt-3">
-      <label for="name">Name:</label>
-      <input type="text" class="form-control" id="name" placeholder="Enter Your Name" name="name">
-    </div>
-    <div class="mb-3 mt-3">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-    <div class="mb-3 mt-3">
-      <label for="phone_no">Phone No:</label>
-      <input type="text" class="form-control" id="phone_no" placeholder="Enter Phone No" name="phone_no">
-    </div>
-    <div class="mb-3 mt-3">
-      <label for="phone_no">Gender:</label>
-     <label for="male">Male <input type="radio" class="form-control" id="male" value="male" name="gender"></label>
-      <label for="female">Female <input type="radio" class="form-control" id="female" value="female" name="gender"></label>
-    </div>
-    <div class="mb-3 mt-3">
-      <label for="designation">Designation:</label>
-      <input type="text" class="form-control" id="designation" placeholder="Enter Designation" name="designation">
-    </div>
-    
-    <div class="mb-3 mt-3">
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-  </form>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8">
+        <h2>Add Employee</h2>
+        <div class="panel panel-primary">
+          <div class="panel-heading">Add Employee</div>
+          <div class="panel-body">
+            <form action="/action_page.php">
+              <div class="mb-3 mt-3">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control" id="name" placeholder="Enter Your Name" name="name">
+              </div>
+              <div class="mb-3 mt-3">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+              </div>
+              <div class="mb-3 mt-3">
+                <label for="phone_no">Phone No:</label>
+                <input type="text" class="form-control" id="phone_no" placeholder="Enter Phone No" name="phone_no">
+              </div>
+              <div class="mb-3 mt-3">
+                <label for="phone_no">Gender:</label>
+                <select name="gender" id="gender">
+                  <option value="">Select Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              <div class="mb-3 mt-3">
+                <label for="designation">Designation:</label>
+                <input type="text" class="form-control" id="designation" placeholder="Enter Designation"
+                  name="designation">
+              </div>
+
+              <div class="mb-3 mt-3">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
 
 
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
+
+
+
 
 </body>
-</html>
 
+</html>
--- wp-content/plugins/custom-plugin/pages/list-employee.php
+++ wp-content/plugins/custom-plugin/pages/list-employee.php
@@ -1,43 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css">
+
 </head>
+
 <body>
 
-<div class="container mt-3">
-  <h2>List Employees</h2>            
-  <table class="table table-dark table-striped">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-8">
+        <h2>List Employees</h2>
+
+        <div class="panel-heading">List Employee</div>
+        <div class="panel-body">
+          <table class="table table-dark table-striped" id="tbl-employee">
+            <thead>
+              <tr>
+                <th>#ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Designation</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Jhon</td>
+                <td>july@example.com</td>
+                <td>Male</td>
+                <td>PHP Developer</td>
+                <td>
+                  <a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+                  <a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+                  <a href="javascript:void(0)" class="btn btn-info">View</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
+
+  <script>
+    $(function () {
+      new DataTable('#tbl-employee');
+    })
+  </script>
 
 </body>
+
 </html>
